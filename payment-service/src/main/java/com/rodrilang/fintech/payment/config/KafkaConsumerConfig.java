1109e596f9d12861ea93888974a6eedb8b3e1ddc
rodrilang <rodrigolang90mail.com>
2026-06-22 23:34:50 -0300

    cofiguracion reintetos y Dead Letter Queue ante fallos

---
+++ payment-service/src/main/java/com/rodrilang/fintech/payment/config/KafkaConsumerConfig.java
@@ -0,0 +1,31 @@
+package com.rodrilang.fintech.payment.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+@Slf4j
+public class KafkaConsumerConfig {
+
+    @Bean
+    public CommonErrorHandler errorHandler(KafkaTemplate<Object, Object> kafkaTemplate) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
+
+        FixedBackOff backOff = new FixedBackOff(2000L, 3L);
+
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(recoverer, backOff);
+
+        errorHandler.setRetryListeners((consumerRecord, ex, deliveryAttempt) -> {
+            log.warn("Reintento fallido número {} para el mensaje en la partición {}. Error: {}",
+                    deliveryAttempt, consumerRecord.partition(), ex.getMessage());
+        });
+
+        return errorHandler;
+    }
+}
